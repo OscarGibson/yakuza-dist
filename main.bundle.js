6b08176225bcde024e5268d418c32832498c167a
ostap <oneostap@gmail.com>
2018-04-11 23:17:22 +0300

    13

--- main.bundle.js
+++ main.bundle.js
@@ -1223,11 +1223,15 @@ var HeaderComponent = (function () {
             _this.globals.categories_filter = [];
             _this.globals.products = [];
             var tmp_products = {};
-            _this.bottom_nav_list = _this.globals.categories;
+            // this.bottom_nav_list = this.globals.categories;
+            _this.bottom_nav_list = [];
             for (var _i = 0, _a = data['categories']; _i < _a.length; _i++) {
                 var category = _a[_i];
                 _this.globals.categories.push(category);
                 _this.globals.categories_filter.push(category);
+                if (category.is_show == true) {
+                    _this.bottom_nav_list.push(category);
+                }
                 for (var _b = 0, _c = category.products; _b < _c.length; _b++) {
                     var product = _c[_b];
                     tmp_products[product.id] = product;
